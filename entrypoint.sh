8e2e619fa41bef30f0dc1e4069f2b408ffeeaf45
manu <manuel@lonestone.io>
2025-02-18 14:37:35 +0100

    refacto #test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,6 @@
 #!/usr/bin/env sh
 set -e
 
-# Register the runner (non-interactive)
-gitlab-runner register --non-interactive \
-    --url "https://gitlab.com" \
-    --registration-token "$TOKEN" \
-    --executor "docker" \
-    --docker-privileged \
-    --docker-image docker:latest \
-    --description "render runner" \
-    --tag-list "docker,dind" \
-    --run-untagged="true" \
-    --locked="false"
-
-# Start the GitLab Runner service
-gitlab-runner run &
-
-
 # Dummy HTTP server to prevent Render from shutting down
 echo "Starting dummy HTTP server..."
 exec python3 -m http.server 8080
