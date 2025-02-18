e2ba6dd497cce61cc2ec5ceb1855616fd67f4e71
manu <manuel@lonestone.io>
2025-02-18 11:01:09 +0100

    refacto #test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,57 +1,31 @@
-#!/bin/bash
+#!/usr/bin/env sh
+set -e
 
-usermod -aG docker gitlab-runner
+# Check for required environment variables
+if [ -z "$GITLAB_URL" ]; then
+  echo "ERROR: GITLAB_URL is not set."
+  exit 1
+fi
 
-# Ensure Docker daemon runs properly
-RUN service docker start || true
+if [ -z "$GITLAB_TOKEN" ]; then
+  echo "ERROR: GITLAB_TOKEN is not set."
+  exit 1
+fi
 
-while [ $# -gt 0 ]; do
-  case "$1" in
-    --url=*)
-      URL="${1#*=}"
-      ;;
-    --registration-token=*)
-      TOKEN="${1#*=}"
-      ;;
-    --docker-image=*)
-      DOCKER_IMAGE="${1#*=}"
-      ;;
-    --description=*)
-      DESCRIPTION="${1#*=}"
-      ;;
-    --tag-list=*)
-      TAGS="${1#*=}"
-      ;;
-    --run-untagged=*)
-      RUN_UNTAGGED="${1#*=}"
-      ;;
-    --locked=*)
-      LOCKED="${1#*=}"
-      ;;
-    --access-level=*)
-      ACCESS_LEVEL="${1#*=}"
-      ;;
-    *)
-      printf "***************************\n"
-      printf "* Error: Invalid argument.*\n"
-      printf "***************************\n"
-      exit 1
-  esac
-  shift
-done
-
-
-
-# Register the GitLab Runner with privileged mode
+# Register the runner (non-interactive)
 gitlab-runner register --non-interactive \
-  --url "https://gitlab.com/" \
-  --registration-token "${TOKEN}" \
-  --tag-list ${TAGS} \
-  --executor "shell" \
-  --docker-image "docker:latest" \
-  --docker-privileged
+    --url "https://gitlab.com" \
+    --registration-token "$TOKEN" \
+    --executor "docker" \
+    --docker-privileged \
+    --docker-image docker:latest \
+    --description "render runner" \
+    --tag-list "docker,dind" \
+    --run-untagged="true" \
+    --locked="false"
 
-cat /etc/gitlab-runner/config.toml
+# Start the GitLab Runner service
+exec gitlab-runner run --user=gitlab-runner --working-directory=/home/gitlab-runner &
 
 
 # Dummy HTTP server to prevent Render from shutting down
